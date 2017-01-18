7c6f469be4ff2ab678755ec617b3378ffce2d865
Matthew Scofield <matthewgscofield@outlook.com>
2017-01-18 12:38:42 -0600

    baseline functioning project ready for next objectives

---
+++ src/IronYard/Room.java
@@ -0,0 +1,17 @@
+package IronYard;
+
+/**
+ * Created by scofieldservices on 1/18/17.
+ */
+public class Room {
+    int row;
+    int col;
+    boolean wasVisited = false;
+    boolean hasBottom = true;
+    boolean hasRight = true;
+
+    public Room(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
